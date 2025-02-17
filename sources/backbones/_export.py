c0b816461192a9cfcf81a8ae8542b6a2a9555c16
Kurt Stolle <k.h.w.stolle@tue.nl>
2025-02-17 17:03:48 +0100

    Export train/eval modes

--- sources/backbones/_export.py
+++ sources/backbones/_export.py
@@ -1,3 +1,5 @@
+import enum
+
 import torch
 import torch.export
 import torch.nn
@@ -5,8 +7,17 @@ import torch.nn
 __all__ = ["export"]
 
 
+class ExportMode(enum.StrEnum):
+    DEFAULT = enum.auto()
+    TRAINING = enum.auto()
+    INFERENCE = enum.auto()
+
+
 def export(
-    model: torch.nn.Module, *, spatial_shape: tuple[int, int] = (512, 1024)
+    model: torch.nn.Module,
+    *,
+    spatial_shape: tuple[int, int] = (512, 1024),
+    mode: str = ExportMode.DEFAULT,
 ) -> torch.export.ExportedProgram:
     from torch.export.dynamic_shapes import Dim
 
@@ -18,8 +29,18 @@ def export(
     sH = Dim("sH", min=min(128, H // 2), max=min(1024, H * 2))
     sW = Dim("sW", min=min(128, W // 2), max=min(1024, W * 2))
 
+    match mode:
+        case ExportMode.TRAINING:
+            model = model.train()
+        case ExportMode.INFERENCE:
+            model = model.eval()
+        case ExportMode.DEFAULT:
+            pass
+        case _:
+            msg = f"Invalid mode: {mode}"
+            raise ValueError(msg)
+
     return torch.export.export_for_training(
-        model.train(),
         inputs,
         dynamic_shapes=[(sB, Dim.STATIC, 2 * sH, 2 * sW)],  # type: ignore[attr-defined]
     )
